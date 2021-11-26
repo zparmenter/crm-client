3582339c6ed37c8216b84d4017515b6190ce3690
zparmenter <zparmenter20@gmail.com>
2021-11-26 14:20:38 -0600

    added pages for audits and all routing for audits

--- src/config/directions.js
+++ src/config/directions.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-
+/*---------------------------COMPANY PAGE IMPORTS-----------------------------*/
 import CompanyEdit from '../pages/companyPages/CompanyEdit';
 import CompanyCreate from '../pages/companyPages/CompanyCreate';
 import CompanyList from '../pages/companyPages/CompanyList';
 import CompanyShow from '../pages/companyPages/CompanyShow';
+
+/*---------------------------AUDIT PAGE IMPORTS-----------------------------*/
+import AuditList from '../pages/auditPages/AuditList';
+import AuditShow from '../pages/auditPages/AuditShow';
+
 import Home from '../pages/Home';
 
 export default function Directions() {
@@ -16,6 +21,9 @@ export default function Directions() {
             <Route exact path='/companies/:id' element={ <CompanyShow /> } />
             <Route exact path='/companies/:id/companyedit' element={ <CompanyEdit /> } />
             <Route exact path='/companies/createcompany' element={ <CompanyCreate />} />
+
+            <Route exact path='/audits/' element={ <AuditList /> } />
+            <Route exact path='/audits/:id' element={ <AuditShow /> } />
         </Routes>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to GameLib.biz</h1>
+      <h1>Welcome to ...</h1>
+      <Link to={'/companies'}>
+        <button>Clients</button>
+      </Link>
     </div>
   );
 }
