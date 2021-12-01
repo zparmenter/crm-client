0e5b229467b8289f3e4739b8a2aa7d337e7743d0
zparmenter <zparmenter20@gmail.com>
2021-12-01 12:49:13 -0600

    finished styling the audit create form

--- src/pages/auditPages/AuditCreate.js
+++ src/pages/auditPages/AuditCreate.js
@@ -29,123 +29,138 @@ function AuditCreate() {
     }
 
     return (
-        <div>
+        <>
         <Header />
+        <div class='modelDiv'>
         <h2>Audit Form</h2>
-        <form onSubmit={handleSubmit}>
-            <div className='form-input'>
+        <form class='modelForm' onSubmit={handleSubmit}>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='company'>Company</label>
             <input
                 type='text'
                 name='company'
                 onChange={(e) => setCompany(e.target.value)}
                 value={company}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='instagramUrl'>Instagram Url</label>
             <input
                 type='text'
                 name='instagramUrl'
                 onChange={(e) => setInstagramUrl(e.target.value)}
                 value={instagramUrl}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='instagramUrl2'>Instagram Url2</label>
             <input
                 type='text'
                 name='instagramUrl2'
                 onChange={(e) => setInstagramUrl2(e.target.value)}
                 value={instagramUrl2}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='facebookUrl'>Facebook Url</label>
             <input
                 type='text'
                 name='facebookUrl'
                 onChange={(e) => setFacebookUrl(e.target.value)}
                 value={facebookUrl}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='facebookUrl2'>Facebook Url2</label>
             <input
                 type='text'
                 name='facebookUrl2'
                 onChange={(e) => setFacebookUrl2(e.target.value)}
                 value={facebookUrl2}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='twitterUrl'>Twitter Url</label>
             <input
                 type='text'
                 name='twitterUrl'
                 onChange={(e) => setTwitterUrl(e.target.value)}
                 value={twitterUrl}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='twitterUrl2'>Twitter Url2</label>
             <input
                 type='text'
                 name='twitterUrl2'
                 onChange={(e) => setTwitterUrl2(e.target.value)}
                 value={twitterUrl2}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='googleUrl'>Google Url</label>
             <input
                 type='text'
                 name='googleUrl'
                 onChange={(e) => setGoogleUrl(e.target.value)}
                 value={googleUrl}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='googleUrl'>Google Url2</label>
             <input
                 type='text'
                 name='googleUrl'
                 onChange={(e) => setGoogleUrl2(e.target.value)}
                 value={googleUrl2}
+                class='modelFormInput'
             />
             </div>
             
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='phoneNumber'>Phone Number</label>
             <input
                 type='text'
                 name='phoneNumber'
                 onChange={(e) => setNumber(e.target.value)}
                 value={phoneNumber}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='address'>Address</label>
             <input
                 type='text'
                 name='address'
                 onChange={(e) => setAddress(e.target.value)}
                 value={address}
+                class='modelFormInput'
             />
             </div>
-            <div className='form-input'>
+            <div className='form-input modelFormDiv'>
             <label htmlFor='website'>Website</label>
             <input
                 type='text'
                 name='website'
                 onChange={(e) => setWebsite(e.target.value)}
                 value={website}
+                class='modelFormInput'
             />
             </div>
-
-            <input type='submit' value='Save!' />
+            <div class='modelFormDiv'>
+                <input class='submitBtn' type='submit' value='Submit' />
+            </div>
         </form>
         </div>
+        </>
     );
 }
 
